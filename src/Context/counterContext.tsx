ea18624c6557cc7154ed49fc93a8d33250329179
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-09-01 14:28:55 +0200

    documentation context

--- src/Context/counterContext.tsx
+++ src/Context/counterContext.tsx
@@ -3,4 +3,7 @@ import React, { createContext } from "react";
 // Types
 import { CountContextType } from "../@types/types";
 
+/**
+ * Context keeping global count state
+ */
 export const CountContext = createContext<CountContextType>({count: 0, setCount: () => {}});
